084dae7a40625ad11afdb87ad82a7d8f4094a66d
Jun <jenoki@mac.com>
2025-02-25 23:34:57 +0900

    Update Info.lua

--- Info.lua
+++ Info.lua
@@ -1,14 +1,13 @@
 --[[
-Info.lua
-EvalFocus.lrplugin
-Author:@jenoki48
+@file	Info.lua
+@author	remov-b4-flight
 ]]
 
 return {
 
 	LrSdkVersion = 3.0,
 
-	LrToolkitIdentifier = 'nu.mine.ruffles.clearattr',
+	LrToolkitIdentifier = 'cx.ath.remov-b4-flight.clearattr',
 	LrPluginName = 'Clear Attribute',
 	LrPluginInfoUrl='https://twitter.com/remov_b4_flight',
 	LrLibraryMenuItems = { 
